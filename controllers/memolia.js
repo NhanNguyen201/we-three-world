22472702e26a696effb1bb8feb9c0ef466166d33
= <=>
2023-03-27 13:56:36 +0700

    change schema and way to fetch

--- controllers/memolia.js
+++ controllers/memolia.js
@@ -1,7 +1,7 @@
 const { postClient } = require('../sanityClient')
 module.exports.getWorld = async(req, res) => {
     const { _id } = req.query;
-    const userDoc = await postClient.fetch(`*[_type == "user" && id == "${_id}"]{
+    const userDoc = await postClient.fetch(`*[_type == "user" && ( path == "${_id}" || id == "${_id}")]{
         images[]{
             asset->{
                 url
